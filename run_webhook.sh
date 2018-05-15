8062731af260d95d32e8ae140bd3721da9b04322
jerryj <javalove93@gmail.com>
2018-05-15 23:05:49 +0900

    service account key file name

--- run_webhook.sh
+++ run_webhook.sh
@@ -1,5 +1,5 @@
 source ~/python/google/bin/activate
-export GOOGLE_APPLICATION_CREDENTIALS=jerryjg-kr-ce-api-89f4c052ec8e.json
+export GOOGLE_APPLICATION_CREDENTIALS=speech-demo.json
 
 ps -ef | grep python | grep mlapi | awk '{print "kill " $2}' | sh -x
 nohup python mlapi-webhook.py 2>&1 > webhook.log &
